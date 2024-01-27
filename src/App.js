02a76a8f61474cb64da79d2d630b799fa3c29396
KacemMb <mabroukikacem10001@gmail.com>
2024-01-27 16:59:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+  // Initial state with person details, show state, and mounted time
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      person: {
+        fullName: 'John Wick',
+        bio: 'A skilled and determined assassin seeking justice for his beloved dog.',
+        imgSrc: './images/image.jpeg',
+        profession: 'Ex-assassin',
+      },
+      show: false,
+      mountedTime: 0,
+    };
+  // Binding methods to the component instance
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+
+
+// Lifecycle method: runs after the component has been added to the DOM
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        mountedTime: prevState.mountedTime + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+// Toggle the show state when the button is clicked
+  toggleShow() {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  }
+// Render method: defines the structure of the component
+  render() {
+    const { person, show, mountedTime } = this.state;
+
+    return (
+      <div className='App'>
+        <h1>Profile App</h1>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+
+        {show && (
+          <div className='info'>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+
+        <p>Time since mount: {mountedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
